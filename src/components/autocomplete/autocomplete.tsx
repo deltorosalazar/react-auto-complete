6244cd013500f57373c81a7125da203a31ebd241
alexpbt <35373552+alexpbt@users.noreply.github.com>
2024-05-30 16:46:54 -0500

    Creates Autocomplete component
    Creates Autocomplete component and functionality

---
+++ src/components/autocomplete/autocomplete.tsx
@@ -0,0 +1,65 @@
+import React, { ChangeEvent, useState } from "react";
+
+function Autocomplete() {
+    const [inputValue, setInputValue] = useState<string | null>("");
+    const [options, setOptions] = useState<Array<string> | []>([]);
+    const textOptions: Array<string> = ["Colombia", "Estados Unidos", "Francia"];
+
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res => res.json())
+    .then(data => initialize(data))
+    .catch(err => console.log("Error:", err));
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setInputValue(value);
+
+        if (value.length > 0) {
+            const filteredOptions: Array<string> = textOptions.filter((option: string) =>
+                option.toLowerCase().includes(value.toLowerCase())
+            );
+            setOptions(filteredOptions);
+        } else {
+            setOptions([]);
+        }
+    }
+
+    function hadleOptionClick(value: string)  {
+       setInputValue(value as string);
+       setOptions([]);
+    }
+
+    return (
+        <div className="input-container">
+            <input
+                type="text"
+                value={inputValue as string}
+                onChange={handleInputChange}
+                aria-autocomplete="list"
+                aria-controls="autocomplete-list"
+                placeholder="Type your country search"
+            />
+            {options.length > 0 && (
+                <ul className="options-list">
+                    {options.map((option: string, index: number) => (
+                        <li
+                            key={index}
+                            onClick={() => hadleOptionClick(option)}
+                            role="option"
+                            aria-selected
+                        >
+                            {option}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
+}
+
+function initialize(countriesData: Array<string>): Array<string> {
+    countriesData.forEach((country) => console.log());
+    return countriesData;
+}
+
+export default Autocomplete;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="header">
+            <h1>React Auto-complete component</h1>
+            <h2>Try typing any country name</h2>
+        </div>
+    )
+}
+
+export default Header;
